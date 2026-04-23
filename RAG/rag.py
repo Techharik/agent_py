dedd7dccab3258f8c2d517a41df769ef92026cb3
Techharik <khariprasath30@gmail.com>
2026-04-23 21:22:47 +0530

    Feat: Rag python

--- RAG/rag.py
+++ RAG/rag.py
@@ -10,20 +10,22 @@
 # CONTEXT WINDOE(1M TOKEN WINDOW) lIMITED , WE CANNOT INGEST A ALL DATA
 
 # RAG PIPLINE:
-'''
+"""
  1. INFDEXING PHASE - PROVIDE THE DATA.
  2. RETRIVAL PHASE -- CHATTING WITH DATA.
 
 INDEXING Phase:
-User gave a lot of data  --- chunking (split it into smaller peaces) ---> Embedding  model ---> vector embeddings ---> Stored in vector database(chunk and   vectors) 
+User gave a lot of data  --- chunking (split it into smaller peaces) ---> Embedding  model ---> vector embeddings ---> Stored in vector database(chunk and   vectors)
 
 Retrival Phase :
 user - query - vector embeddings - Embedding Models -- Vector embedding (query) --> Vector similarty search in db - got the relvent chucks from the dbs (vector - content) -->
- Pass the data to the system prompt. 
-  -- lllm - user query data it return output 
-'''
+ Pass the data to the system prompt.
+  -- lllm - user query data it return output
+"""
 
 # WE Need Vector database. - pinecone -weaviate - chromadb - pgvector -qdrant db
 
 # Qdrant
 # LangChain - Having all the abstract code we can use the prebuild functions.
+
+print("Rag Demo")
